46368bd1877e2e5a87b1dd75a24f1a72621c19eb
EDSON <edsonwasswa54@gmail.com>
2025-05-23 11:02:28 +0300

    Made the login page

--- src/main/java/com/nursing/management/auth/DatabaseConnector.java
+++ src/main/java/com/nursing/management/auth/DatabaseConnector.java
@@ -2,14 +2,21 @@ package com.nursing.management.auth;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DatabaseConnector {
-    private static final String URL = "jdbc:mysql://localhost:3306/nursing_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
+public static Connection connectDb() {
+	try {
+		class.forName("com.myql.jdbc.Driver");
+		Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:")
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+	}
+	
 }
+
+
+
+//private static final String URL = "jdbc:mysql://localhost:3306/nursing_db";
+//private static final String USER = "root";
+//private static final String PASSWORD = "";
