3278e265bf788f2eee02b626a4907a2acfa648c3
EDSON <edsonwasswa54@gmail.com>
2025-05-27 04:36:36 +0300

    Project-Rakai

--- src/main/java/com/nursing/management/auth/DatabaseConnector.java
+++ src/main/java/com/nursing/management/auth/DatabaseConnector.java
@@ -1,23 +1,17 @@
 package com.nursing.management.auth;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-/*
-public class DatabaseConnector {
-public static Connection connectDb() {
-	try {
-		class.forName("com.myql.jdbc.Driver");
-		Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:");
-	}catch(Exception e) {
-		e.printStackTrace();
-	}
-	}
-	
+
+public class DatabaseConnector{
+	 public static Connection connectDb(){
+		 try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connect = DriverManager.getConnection("jdbc:mysql:localhost/logindata", "root","");
+			return connect;
+		 }catch(Exception e) {
+			 e.printStackTrace();
+			
+		 }
+		return null;
+	 }
 }
-*/
-
-
-
-//private static final String URL = "jdbc:mysql://localhost:3306/nursing_db";
-//private static final String USER = "root";
-//private static final String PASSWORD = "";
