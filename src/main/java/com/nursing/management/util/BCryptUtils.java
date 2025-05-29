ba4ab579ef45ff4562e1769d833f4c6da8b445a6
EDSON <edsonwasswa54@gmail.com>
2025-05-29 04:59:29 +0300

    You can login from Signin to Dashbord

--- src/main/java/com/nursing/management/util/BCryptUtils.java
+++ src/main/java/com/nursing/management/util/BCryptUtils.java
@@ -2,7 +2,7 @@ package com.nursing.management.util;
 
 import org.mindrot.jbcrypt.BCrypt;
 
-public class BCryptUtil{
+public interface BCryptUtils{
 	public static String hashPassword(String plainPassword) {
 		return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
 	}
