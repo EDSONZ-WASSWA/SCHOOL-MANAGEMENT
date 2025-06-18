af0bdac52a3568817a3c46b77663d9137be7af0e
Elvis K <katoelvis23@gmail.com>
2025-06-19 01:40:32 +0300

    THE LAST PUSH -@KELVIS

--- src/main/java/com/nursing/management/Main.java
+++ src/main/java/com/nursing/management/Main.java
@@ -1,10 +1,13 @@
  package com.nursing.management;
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
@@ -12,10 +15,14 @@ public class Main extends Application {
 	private double y = 0;
 
 	@Override
-
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
 		Scene scene = new Scene(root);
+		stage.setTitle("LOGIN PAGE");
+		Image image = new Image("/images/RCSN.jpg"); //Adding the Icon
+		stage.getIcons().add(image);
+		stage.centerOnScreen();
+		
 		root.setOnMousePressed((MouseEvent event) -> {
 			x = event.getSceneX();
 			y = event.getSceneY();
@@ -32,9 +39,10 @@ public class Main extends Application {
 		stage.setScene(scene);
 		stage.show();
 	}
+
+
 	public static void main(String[] args) {
 		System.setProperty("prism.marlinrasterizer", "false"); // This line removes the marlin warnings in the Terminal
-		launch (args);
+		launch(args);
 	}
 }
-
